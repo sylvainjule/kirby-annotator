f596041fbc30921b98469d8de703b6e9d42a9118
Sylvain Julé <sylvain.jule@free.fr>
2018-10-02 22:56:23 +0200

    Return different styles depending on the marker type

--- lib/functions.php
+++ lib/functions.php
@@ -3,7 +3,11 @@
 function markerStyle($marker) {
 	$style =  'left: '. $marker->x()->toPercentString() .';';
 	$style .= 'top: '. $marker->y()->toPercentString() .';';
-	$style .= 'width: '. $marker->w()->toPercentString() .';';
-	$style .= 'height: '. $marker->h()->toPercentString() .';';
+
+	if($marker->type()->isNotPin()) {
+		$style .= 'width: '. $marker->w()->toPercentString() .';';
+		$style .= 'height: '. $marker->h()->toPercentString() .';';
+	}
+	
 	return $style;
 }
